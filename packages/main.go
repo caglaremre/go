6425c0652e8e808daf207faedf805e2f271034dc
emre çağlar <caglar.emre01@gmail.com>
2024-06-11 22:45:21 +0300

    finished packages

--- packages/main.go
+++ packages/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/caglaremre/myniceprogram/helpers"
+)
 
 func main() {
 	log.Println("Hello")
+
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some name"
+	log.Println(myVar.TypeName)
+
 }
