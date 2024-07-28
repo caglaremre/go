229c7740667b1ab00f452860ce87c9231f70cb3a
emre çağlar <caglar.emre01@gmail.com>
2024-07-28 14:39:22 +0300

    sessions and more config

--- web_application/cmd/web/main.go
+++ web_application/cmd/web/main.go
@@ -7,13 +7,29 @@ import (
 	"mywebapp/pkg/handlers"
 	"mywebapp/pkg/render"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portnumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application function
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
@@ -33,7 +49,7 @@ func main() {
 		Addr:    portnumber,
 		Handler: routes(&app),
 	}
-	
+
 	err = srv.ListenAndServe()
 	log.Fatal(err)
 }
