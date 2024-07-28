59adc57b326de6febf23c0674679607a81eaf0c7
emre çağlar <caglar.emre01@gmail.com>
2024-07-28 15:50:29 +0300

    copied from previous web app

---
+++ bookings/cmd/web/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bookings/pkg/config"
+	"bookings/pkg/handlers"
+	"bookings/pkg/render"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portnumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Printf("starting application on %s\n", portnumber)
+
+	srv := &http.Server{
+		Addr:    portnumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
